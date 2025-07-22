4ac462cf1011d9132da5b0349823c1da0f003a4d
Olira1 <oliratesgera1@gmail.com>
2025-07-22 21:01:50 +0300

    second commit

--- CALCULATOR/src/Component/Calculator.jsx
+++ CALCULATOR/src/Component/Calculator.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React,{useState} from 'react';
+import  './Calculator.css'
 
 const Calculator = () => {
+    const [input, setInput] = useState("")
+
+    const handleClick = (value) => {
+        setInput(input + value)
+    }
+
+    const clear = () => setInput("")
+
+    const calculate = () => {
+        try {
+            setInput(eval(input).toString());
+        } catch (error) {
+            setInput("Error")
+        }
+    }
+
+
     return (
-        <div>
-            <h1>hello</h1>
+      <div className="calculator">
+        <div className="display">{input || "0"}</div>
+        <div className="buttons">
+          <button className="operator" onClick={() => handleClick("*")}>
+            *
+          </button>
+          <button className="operator" onClick={() => handleClick("/")}>
+            /
+          </button>
+          <button className="operator" onClick={() => handleClick("-")}>
+            -
+          </button>
+          <button className="operator" onClick={() => handleClick("+")}>
+            +
+          </button>
+
+          {[9, 8, 7, 6, 5, 4, 3, 2].map((num) => (
+            <button key={num} onClick={() => handleClick(num)}>
+              {num}
+            </button>
+          ))}
+
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>.</button>
+          <button className="equal" onClick={() => calculate()}>
+            =
+          </button>
+          <button className="clear" onClick={() => clear()}>
+            C
+          </button>
         </div>
+      </div>
     );
 }
 
